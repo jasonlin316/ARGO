70fc72e80d92869354fc06dcc77ce192b65a89ff
Yi-Chien Lin <jasonlin316@gmail.com>
2023-10-05 18:07:26 -0700

    Update ogb_example.py

--- ogb_example.py
+++ ogb_example.py
@@ -1,5 +1,5 @@
 '''
-Disclaimer: we did not write or own this code. 
+Disclaimer: we do not write or own this code. 
 This file is an official example from DGL: https://github.com/dmlc/dgl/blob/master/examples/pytorch/ogb/ogbn-products/graphsage/main.py
 '''
 
